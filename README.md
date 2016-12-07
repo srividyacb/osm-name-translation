04f03e551e48690c87b141cd314873fb13bbd2d6
Srividya <srividya@mapbox.com>
2016-12-07 13:50:59 +0530

    made new modifications to upload script

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 ## tilereduce-translation
 
-Localization of `name` tags using tilereduce
+### Localization of `name` tags using tilereduce
 - Uses tilereduce - [osm-tag-stats](https://github.com/mapbox/osm-tag-stats) to extract `name` tags which don't have `name:kn tags` (see `tags.json` file for filters).
 - Uses geojson output of tilereduce and converts to csv for easy translation.
 - Uses [osmapi](http://osmapi.metaodi.ch/) a python module to upload the changed `name:kn` tags to OPenStreetMap. (add user credentials in `upload.py` script)
+
+
+### Upload to OpenStreetMp from `csv`
+
+**Arguments required**
+
+1. inputFile : Input `csv` file with translations
+2. inputLanguage : Language-code tag to which the translations are updated (example `name:kn`)
+3. logFile : A log file which logs all the activities
+
+**How to execute**
+
+```
+python upload.py <inputfile.csv> <languagecode> <outputLog>
+```
--- upload.py
+++ upload.py
@@ -1,21 +1,70 @@
+# -*- coding: utf-8 -*-
 import osmapi
 import sys
 import csv
+import json
 
-api = osmapi.OsmApi(username = u"username", password = u"****")
-api.ChangesetCreate({u"comment": u"adding kannada name tags"})
+inputFile = sys.argv[1]
+inputLanguage = sys.argv[2]
+log = sys.argv[3]
 
-with open('translate.csv', 'rb') as csvfile:
-    # csv.reader(csvfile)
-    for row in csvfile:
-        osm_id = row.split(',')[0]
-        translation = row.split(',')[3]
-        translation = translation.decode("utf-8")
-        try:
-            node = api.NodeGet(int(osm_id))
-        except ValueError:
+api = osmapi.OsmApi(api="api06.dev.openstreetmap.org", username = "stest", password = "yspasswdys")
+api.ChangesetCreate({u"comment": u"adding kn name tags"})
+
+def get_osm_type (osm_id, osm_type):
+    if osm_type == 'node':
+        node = api.NodeGet(int(osm_id))
+        return node
+    elif osm_type == 'way':
+        way = api.WayGet(int(osm_id))
+        return way
+    elif osm_type == 'relation':
+        relation = api.RelationGet(osm_id)
+        return relation
+    else:
+        print "invalid type %s" % osm_type
+
+def osm_upload (osm_type, osm_object):
+    if osm_type == 'node':
+        node = api.NodeUpdate(osm_object)
+        return node
+    elif osm_type == 'way':
+        way = api.WayUpdate(osm_object)
+        return way
+    elif osm_type == 'relation':
+        relation = api.RelationUpdate(osm_object)
+        return relation
+
+with open(inputFile, 'r') as csvfile:
+    logFile = open(log, 'w')
+    rows = csv.reader(csvfile, delimiter=',')
+    i = 0
+    for row in rows:
+        if i == 0:
+            i = i+1
             continue
-        print node
-        node["tag"]["name:kn"] = translation
-        node = api.NodeUpdate(node)
+        osm_id = row[0]
+        osm_type = str(row[1])
+        translation = str(row[2]).strip('\n')
+        translation = translation.decode('utf-8')
+        try:
+            typeContent = get_osm_type (osm_id, osm_type)
+            if ('name:'+ inputLanguage in typeContent['tag']):
+                osmContent = typeContent['tag']['name:'+ inputLanguage]
+                if osmContent == translation:
+                    print (osmContent, translation)
+                    print ('Translation present in OpenStreetMap')
+                    logFile.write('Translation present in OSM' + '\n')
+                else:
+                    print (osmContent, translation)
+                    print ('MisMatched Translation in OpenStreetMap')
+                    logFile.write('MisMatched Translation in OpenStreetMap' +'\n')
+            else:
+                typeContent['tag']['name:'+ inputLanguage] = translation
+                osm_upload(osm_type, typeContent)
+                print ('Uploaded new translation to OpenStreetMap')
+                logFile.write('Uploaded new translation to OpenStreetMap' + '\n')
+        except ValueError:
+            print "ValueError occurred"
+            continue
 api.ChangesetClose()
